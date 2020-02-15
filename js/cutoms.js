f6a9a19a13d595b914929ccfaa4d75bab3db68c2
ricks774 <ricks774@gmail.com>
2020-02-16 04:08:46 +0800

    up load

---
+++ js/cutoms.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+	// fullpage plugin
+	$('#fullpage').fullpage({
+		anchors:['firstPage', 'secondPage', 'thirdPage', '4thPage', '5thPage'],
+		menu: '#js-menu',
+		autoScrolling: false,
+		fitToSection: false,
+		scrollingSpeed: 1200
+	});
+
+	//logo change when scroll
+	$(window).on('scroll',function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if (scrollDistance > 80) {
+			$header.addClass("header--scrolling");
+		}
+		else{
+			$header.removeClass("header--scrolling");
+		}
+	})
+
+	//RALLAX
+	if (screen.width > 768) {
+		var rellax = new Rellax('.rellax', {
+	    center: true
+		});
+	}
+
+
+
+	//owl carousel 
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:16,
+	    nav:true,
+	    navText:[],
+	    dots: false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	})
+});
